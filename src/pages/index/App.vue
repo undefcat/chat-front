f3c5c72c763b89501adf2f5897ffc08ccf907f67
undefcat <undefcat@gmail.com>
2019-07-21 23:52:56 +0900

    닉네임 입력 추가

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,7 +1,35 @@
 <template>
   <div id="main">
     <h1>Open Chat</h1>
-    <ChatRoomList :rooms="rooms"/>
+    <ChatRoomList />
+    <el-dialog
+        title="닉네임 설정"
+        width="35%"
+        :visible.sync="isOpen"
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+        :show-close="false"
+        v-loading="isRequest">
+      <el-form :model="form" @submit.native.prevent>
+        <el-form-item
+            label="이름"
+            label-width="120px">
+          <el-input
+              v-model="form.name"
+              autocomplete="off"
+              @keydown.native.enter="setName"/>
+        </el-form-item>
+      </el-form>
+      <div
+          slot="footer"
+          class="dialog-footer">
+        <el-button
+            type="primary"
+            @click="setName">
+          확인
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -12,17 +40,35 @@
     components: { ChatRoomList },
     data() {
       return {
-        rooms: [],
+        isRequest: false,
+        form: {
+          name: '',
+        }
+      }
+    },
+    computed: {
+      isOpen() {
+        return this.$store.state.user.name === ''
       }
     },
     methods: {
       showCreateRoomDialog(rooms) {
         this.rooms = rooms
+      },
+      setName() {
+        const name = this.form.name.trim()
+        if (name.length < 2 || name.length > 16) {
+          this.$message({
+            type: 'error',
+            message: '이름은 2글자 ~ 16글자여야 합니다.',
+          })
+          return
+        }
+
+        this.$store.dispatch('setName', { name: this.form.name })
+        this.isRequest = true
       }
     },
-    mounted() {
-      this.$store.dispatch('setName', {name: 'name'})
-    }
   }
 </script>
 
