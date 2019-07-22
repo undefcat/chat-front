96f6965127b09a5962f67ae878a68c774e5468e4
undefcat <undefcat@gmail.com>
2019-07-22 15:32:38 +0900

    build 파일 경로 설정

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  publicPath: '/dist',
   devServer: {
     proxy: {
       '^/ws': {
         target: 'http://localhost:8000',
         ws: true,
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 }
